4ed21b0f33d5008fd7f5583ed517aa58adda01c8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:25:50 +0200

    cambiamenti

--- ScontoSupermercato/Program.cs
+++ ScontoSupermercato/Program.cs
@@ -6,7 +6,20 @@ namespace ScontoSupermercato
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci il totale della spesa");
+            double a = double.Parse(Console.ReadLine());
+            double s = (a * 20) / 100;
+            if (a < 100)
+            {
+                Console.WriteLine($"l'importo è {a} ");
+                Console.ReadLine();
+            }
+            else
+            {
+                double importo = a - s;
+                Console.WriteLine($"l'importo scontato è {importo:c}");
+                Console.ReadLine();
+            }
         }
     }
 }
